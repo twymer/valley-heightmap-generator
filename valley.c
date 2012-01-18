86937f628d0b77527f8996f92f844c87b1b16a28
Tyler Wymer <twymer@gmail.com>
2012-01-18 18:36:37 -0500

    Change this message

--- valley.c
+++ valley.c
@@ -1,17 +1,21 @@
 #include "SDL/SDL.h"
-#include <stdlib.h> /* Required for random */
 #include <time.h> /* For random seeding */
 #include <math.h> /* For powers in random gen */
+#include <stdio.h>
 
 #include <GL/gl.h>
 #include <GL/glu.h>
 
+#include <libnoise/noise.h>
+#include "noiseutils.h"
+
+#define DEPTH 5
 #define W_WIDTH 640
 #define W_HEIGHT 480
 
-#define DEPTH 5
-
 #define REDUCE_RANGE_CONSTANT .5
+
+using namespace noise;
 
 typedef struct _point {
     int x;
@@ -33,7 +37,7 @@ point* perturb_point(point* points, int before, int after, int depth) {
     //printf("Currently at depth %i.\n", depth);
 
     point* midpoint;
-    midpoint = malloc(sizeof(point));
+    midpoint = (point*)malloc(sizeof(point));
     midpoint->x = (points[before].x + points[after].x) / 2;
     midpoint->y = (points[before].y + points[after].y) / 2;
 
@@ -54,32 +58,99 @@ point* perturb_point(point* points, int before, int after, int depth) {
 point* form_line(int depth, point start, point end) {
     int total_points = depth * depth;
     point* points;
-    points = malloc(sizeof(point) * total_points);
+    points = (point*)malloc(sizeof(point) * total_points);
     points[0] = start;
     points[total_points - 1] = end;
 
     return perturb_point(points, 0, total_points - 1, 0);
 }
 
-main(int argc, char* argv[]) {
-    if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
-        printf("Failed\n");
-        return;
-    }
+float valley_function(int x) {
+    return x * x + x;
+}
+
+// http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
+float distance_to_line(point p0, point p1, point p2) {
+    float d = (p2.x - p1.x) * (p1.y - p0.y) - (p1.x - p0.x) * (p2.y - p1.y);
+    d = abs(d) / sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    return d;
+}
+
+int main() {
+    // Initialize noise objects
+    module::Perlin perlin;
+    perlin.SetOctaveCount(6);
+    perlin.SetFrequency(1.0);
+    perlin.SetFrequency(0.5);
+
+    utils::NoiseMap height_map;
+    utils::NoiseMapBuilderPlane height_map_builder;
+    height_map_builder.SetSourceModule(perlin);
+    height_map_builder.SetDestNoiseMap(height_map);
+    height_map_builder.SetDestSize(256, 256);
+    height_map_builder.SetBounds(2.0, 6.0, 1.0, 5.0);
+    height_map_builder.Build();
+
+    // Do work
 
     /* random seed */
     srand((unsigned int)time(NULL));
 
+    // Setup line
     point start, end;
     start.x = 5;
-    start.y = 200;
-    end.x = 600;
-    end.y = 200;
+    start.y = 100;
+    end.x = 250;
+    end.y = 100;
     point* points;
     int total_points = DEPTH * DEPTH;
     points = form_line(DEPTH, start, end);
 
+    int x, y;
+    for(x = 0; x < 512; x++) {
+        for(y = 0; y < 512; y++) {
+            point current_point;
+            current_point.x = x;
+            current_point.y = y;
+            float min_dist = 99999;
+
+            // Find out how close this point is to a line
+            int i;
+            for(i = 0; i < total_points; i++) {
+                point p0 = points[i];
+                point p1 = points[i+1];
+                float dist = minimum_distance(current_point, p0, p1);
+                if(dist < min_dist) {
+                    min_dist = dist;
+                }
+            }
+
+            if(min_dist < 5) {
+                height_map.SetValue(x, y, 0);
+            } else {
+                printf("min dist: %f, x: %i, y: %i\n", min_dist, x, y);
+            }
+        }
+    }
+
+    // Render the heightmap
+    utils::RendererImage renderer;
+    utils::Image image;
+    renderer.SetSourceNoiseMap(height_map);
+    renderer.SetDestImage(image);
+    renderer.Render();
+
+    utils::WriterBMP writer;
+    writer.SetSourceImage(image);
+    writer.SetDestFilename("heightmap.bmp");
+    writer.WriteDestFile();
+
     /* HERE BE OPENGL DRAGONS */
+
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+        printf("Failed\n");
+        return 0;
+    }
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_Surface* screen = SDL_SetVideoMode(W_WIDTH, W_HEIGHT, 16, SDL_OPENGL | SDL_RESIZABLE);
@@ -110,8 +181,4 @@ main(int argc, char* argv[]) {
 
     SDL_GL_SwapBuffers();
     SDL_Delay(3000);
-
-    /* cleanup */
-
-    free(points);
 }
