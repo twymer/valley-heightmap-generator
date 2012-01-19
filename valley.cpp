1222f29edb0d148daec78a100f199bdce0a82cc2
Tyler Wymer <twymer@gmail.com>
2012-01-19 00:50:41 -0500

    Some rough path carving

--- valley.cpp
+++ valley.cpp
@@ -17,10 +17,31 @@
 
 using namespace noise;
 
-typedef struct _point {
-    int x;
-    int y;
-} point;
+typedef struct _vec2 {
+    float x;
+    float y;
+} vec2;
+
+vec2 operator-(const vec2 &v0, const vec2 &v1) {
+    vec2 v2;
+    v2.x = v0.x - v1.x;
+    v2.y = v0.y - v1.y;
+    return v2;
+}
+
+vec2 operator+(const vec2 &v0, const vec2 &v1) {
+    vec2 v2;
+    v2.x = v0.x + v1.x;
+    v2.y = v0.y + v1.y;
+    return v2;
+}
+
+vec2 operator*(const float &f, const vec2 &v) {
+    vec2 product;
+    product.x = f * v.x;
+    product.y = f * v.y;
+    return product;
+}
 
 float random_by_depth(int depth) {
     float reduce_by = pow(REDUCE_RANGE_CONSTANT, depth + 1);
@@ -29,15 +50,15 @@ float random_by_depth(int depth) {
     return max * random;
 }
 
-point* perturb_point(point* points, int before, int after, int depth) {
+vec2* perturb_point(vec2* points, int before, int after, int depth) {
     if(depth >= DEPTH) {
         return points;
     }
     //printf("Perturbing point between %i and %i.\n", before, after);
     //printf("Currently at depth %i.\n", depth);
 
-    point* midpoint;
-    midpoint = (point*)malloc(sizeof(point));
+    vec2* midpoint;
+    midpoint = (vec2*)malloc(sizeof(vec2));
     midpoint->x = (points[before].x + points[after].x) / 2;
     midpoint->y = (points[before].y + points[after].y) / 2;
 
@@ -55,10 +76,10 @@ point* perturb_point(point* points, int before, int after, int depth) {
     return points;
 }
 
-point* form_line(int depth, point start, point end) {
+vec2* form_line(int depth, vec2 start, vec2 end) {
     int total_points = depth * depth;
-    point* points;
-    points = (point*)malloc(sizeof(point) * total_points);
+    vec2* points;
+    points = (vec2*)malloc(sizeof(vec2) * total_points);
     points[0] = start;
     points[total_points - 1] = end;
 
@@ -66,14 +87,35 @@ point* form_line(int depth, point start, point end) {
 }
 
 float valley_function(int x) {
-    return x * x + x;
+    return .1 * x;
 }
 
-// http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
-float distance_to_line(point p0, point p1, point p2) {
-    float d = (p2.x - p1.x) * (p1.y - p0.y) - (p1.x - p0.x) * (p2.y - p1.y);
-    d = abs(d) / sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    return d;
+float length_squared(vec2 v, vec2 w) {
+    return (w.x - v.x) * (w.x - v.x) + (w.y - v.y) * (w.y - v.y);
+}
+
+float distance(vec2 p0, vec2 p1) {
+    return abs(p0.x - p1.x) + abs(p0.y - p1.y);
+}
+
+float dot(vec2 v, vec2 w) {
+    return v.x * w.x + v.y * w.y;
+}
+
+// http://mathworld.wolfram.com/vec2-LineDistance2-Dimensional.html
+// http://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
+float distance_to_line(vec2 v, vec2 w, vec2 p) {
+    // Return minimum distance between line segment vw and vec2 p
+    const float l2 = length_squared(v, w);  // i.e. |w-v|^2 -  avoid a sqrt
+    if (l2 == 0.0) return distance(p, v);   // v == w case
+    // Consider the line extending the segment, parameterized as v + t (w - v).
+    // We find projection of vec2 p onto the line. 
+    // It falls where t = [(p-v) . (w-v)] / |w-v|^2
+    const float t = dot(p - v, w - v) / l2;
+    if (t < 0.0) return distance(p, v);       // Beyond the 'v' end of the segment
+    else if (t > 1.0) return distance(p, w);  // Beyond the 'w' end of the segment
+    const vec2 projection = v + t * (w - v);  // Projection falls on the segment
+    return distance(p, projection);
 }
 
 int main() {
@@ -97,38 +139,41 @@ int main() {
     srand((unsigned int)time(NULL));
 
     // Setup line
-    point start, end;
+    vec2 start, end;
     start.x = 5;
     start.y = 100;
     end.x = 250;
     end.y = 100;
-    point* points;
+    vec2* points;
     int total_points = DEPTH * DEPTH;
     points = form_line(DEPTH, start, end);
 
     int x, y;
     for(x = 0; x < 512; x++) {
         for(y = 0; y < 512; y++) {
-            point current_point;
+            vec2 current_point;
             current_point.x = x;
             current_point.y = y;
             float min_dist = 99999;
 
             // Find out how close this point is to a line
             int i;
-            for(i = 0; i < total_points; i++) {
-                point p0 = points[i];
-                point p1 = points[i+1];
-                float dist = minimum_distance(current_point, p0, p1);
+            for(i = 0; i < total_points - 1; i++) {
+                vec2 p0 = points[i];
+                vec2 p1 = points[i+1];
+                float dist = distance_to_line(p0, p1, current_point);
                 if(dist < min_dist) {
                     min_dist = dist;
                 }
             }
 
-            if(min_dist < 5) {
-                height_map.SetValue(x, y, 0);
-            } else {
+            if(min_dist < 15) {
+                float previous_value = height_map.GetValue(x, y);
+                float function_offset = valley_function(min_dist);
                 printf("min dist: %f, x: %i, y: %i\n", min_dist, x, y);
+                printf("function_offset: %f\n\n", function_offset);
+                printf("previous value: %f\n\n", previous_value);
+                height_map.SetValue(x, y, previous_value - function_offset);
             }
         }
     }
